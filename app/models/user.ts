8870725e04fd5af7865639e0747e0dffaafa6026
Hitarashi <sayeed205@gmail.com>
2024-05-31 18:13:12 +0530

    Add user schema and migration

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,8 +1,9 @@
-import { DateTime } from 'luxon'
-import hash from '@adonisjs/core/services/hash'
-import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { compose } from '@adonisjs/core/helpers'
+import hash from '@adonisjs/core/services/hash'
+import { BaseModel, beforeCreate, column } from '@adonisjs/lucid/orm'
+import { DateTime } from 'luxon'
+import { v4 as uuid } from 'uuid'
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
@@ -10,11 +11,13 @@ const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
 })
 
 export default class User extends compose(BaseModel, AuthFinder) {
+  static selfAssignPrimaryKey = true
+
   @column({ isPrimary: true })
-  declare id: number
+  declare id: string
 
   @column()
-  declare fullName: string | null
+  declare name: string | null
 
   @column()
   declare email: string
@@ -27,4 +30,9 @@ export default class User extends compose(BaseModel, AuthFinder) {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime | null
+
+  @beforeCreate()
+  public static async assignUUID(user: User) {
+    user.id = user.id || uuid()
+  }
 }
--- database/migrations/1717157567484_create_users_table.ts
+++ database/migrations/1717157567484_create_users_table.ts
@@ -5,8 +5,8 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').notNullable()
-      table.string('full_name').nullable()
+      table.uuid('id').primary()
+      table.string('name').nullable()
       table.string('email', 254).notNullable().unique()
       table.string('password').notNullable()
 
